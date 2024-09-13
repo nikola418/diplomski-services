5d0007a8917aa2110e50248e9025d5ed3cc51f36
Nikola Milovanovic <nikolam998@gmail.com>
2024-09-13 22:09:33 +0200

    save

--- apps/users/src/users/users.module.ts
+++ apps/users/src/users/users.module.ts
@@ -1,10 +1,16 @@
+import { GridFsMulterConfigService } from '@libs/data-access-files';
 import { DataAccessUsersModule } from '@libs/data-access-users';
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { FavoriteLocationsModule } from './favorite-locations/favorite-location.module';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [DataAccessUsersModule, FavoriteLocationsModule],
+  imports: [
+    MulterModule.registerAsync({ useClass: GridFsMulterConfigService }),
+    DataAccessUsersModule,
+    FavoriteLocationsModule,
+  ],
   controllers: [UsersController],
 })
 export class UsersModule {}
