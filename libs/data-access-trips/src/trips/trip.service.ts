2f991f8dc57395ab12c898e4a3ded2b17b891968
Nikola Milovanovic <nikolam998@gmail.com>
2024-09-25 02:01:58 +0200

    idk

--- libs/data-access-trips/src/trips/trip.service.ts
+++ libs/data-access-trips/src/trips/trip.service.ts
@@ -14,6 +14,7 @@ export class TripService {
   private static readonly include: Prisma.TripInclude = {
     location: true,
     tripAttendances: { include: { user: true } },
+    chatGroup: true,
   };
   private static readonly orderBy: Prisma.TripOrderByWithRelationInput = {};
 
